0c8882b7ba110c3d8fea05ce43734b64786c9f72
Mvcampbell3 <mvcampbell3@gmail.com>
2019-08-13 17:46:52 -0400

    adding in animation for board

--- public/assets/js/home.js
+++ public/assets/js/home.js
@@ -1,23 +1,75 @@
 const box2 = document.getElementById("box2");
-let paused = false;
+let box2Animation;
+went = false;
 
-const box2Animation = box2.animate({
-  transform: ["translate(0px,0px) rotateZ(0deg)", "translate(100px, 0px) rotateZ(45deg)"],
-  // opacity: [0, 1],
-  boxShadow: ["0px 0px 0px black", "4px 4px 4px black", "12px 12px 12px #333"],
-  borderRadius: ["0px", "1%","50%"]
-}, {
-    easing: "linear",
-    duration: 1000,
-    iterations: "Infinity",
-    direction: "alternate"
-  })
+let turn = 0;
 
-box2.addEventListener("click", () => {
-  if (!paused) {
-    box2Animation.pause();
-    return paused = true;
+function playAnimation() {
+  const moveArr = settleP();
+
+  box2Animation = box2.animate({
+    transform: [`translate(${moveArr[0]}px, ${moveArr[2]}px)`, `translate(${moveArr[1]}px, ${moveArr[3]}px)`]
+  }, {
+      easing: "linear",
+      duration: moveArr[4],
+      fill: "forwards"
+    })
+  box2Animation.onfinish = function() {
+    if (turn <= 4) {
+      box2.addEventListener("click", playAnimation, { once: true })
+    } else {
+      console.log("no more")
+    }
   }
-  box2Animation.play();
-  paused = false;
-})
+}
+
+function settleP() {
+  let xp1, xp2, yp1, yp2, times;
+  switch (turn) {
+    case 0:
+      xp1 = -110;
+      xp2 = -110;
+      yp1 = 0;
+      yp2 = 300;
+      times = 1000;
+      turn++;
+      break;
+    case 1:
+      xp1 = -110;
+      xp2 = -55;
+      yp1 = 300;
+      yp2 = 300;
+      times = 333;
+      turn++;
+      break;
+    case 2:
+      xp1 = -55;
+      xp2 = -55;
+      yp1 = 300;
+      yp2 = 0;
+      times = 1000;
+      turn++;
+      break;
+    case 3:
+      xp1 = -55;
+      xp2 = -0;
+      yp1 = 0;
+      yp2 = 0;
+      times = 333;
+      turn++;
+      break;
+    case 4:
+      xp1 = 0;
+      xp2 = 0;
+      yp1 = 0;
+      yp2 = 300;
+      times = 1000;
+      turn++;
+      break;
+    default:
+      console.log("switch did not work");
+  }
+  return [xp1, xp2, yp1, yp2, times];
+}
+
+box2.addEventListener("click", playAnimation, { once: true })
